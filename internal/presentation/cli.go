9847be9a3058c64f51cd4688ed172bfcf4729ae7
Jonathan ARNAULT <jonathan@marmelab.com>
2023-02-20 17:55:30 +0100

    Display board to console

--- internal/presentation/cli.go
+++ internal/presentation/cli.go
@@ -1,4 +1,115 @@
 package presentation
 
+import (
+	"fmt"
+	"io"
+
+	"github.com/marmelab/labyrinth/internal/model"
+)
+
+// This is the shape parts for line assembly
+var shapesParts = map[model.Shape](map[model.Rotation](map[int]string)){
+	model.ShapeI: map[model.Rotation](map[int]string){
+		model.Rotation0: map[int]string{
+			0: "───",
+			1: ".%s.",
+			2: "───",
+		},
+		model.Rotation90: map[int]string{
+			0: "│.│",
+			1: "│%s│",
+			2: "│.│",
+		},
+		model.Rotation180: map[int]string{
+			0: "───",
+			1: ".%s.",
+			2: "───",
+		},
+		model.Rotation270: map[int]string{
+			0: "│.│",
+			1: "│%s│",
+			2: "│.│",
+		},
+	},
+	model.ShapeT: map[model.Rotation](map[int]string){
+		model.Rotation0: map[int]string{
+			0: "┘.└",
+			1: ".%s.",
+			2: "───",
+		},
+		model.Rotation90: map[int]string{
+			0: "│.└",
+			1: "│%s.",
+			2: "│.┌",
+		},
+		model.Rotation180: map[int]string{
+			0: "───",
+			1: ".%s.",
+			2: "┐.┌",
+		},
+		model.Rotation270: map[int]string{
+			0: "┘.│",
+			1: ".%s│",
+			2: "┐.│",
+		},
+	},
+	model.ShapeV: map[model.Rotation](map[int]string){
+		model.Rotation0: map[int]string{
+			0: "──┐",
+			1: ".%s│",
+			2: "┐.│",
+		},
+		model.Rotation90: map[int]string{
+			0: "┘.│",
+			1: ".%s│",
+			2: "──┘",
+		},
+		model.Rotation180: map[int]string{
+			0: "│.└",
+			1: "│%s.",
+			2: "└──",
+		},
+		model.Rotation270: map[int]string{
+			0: "┌──",
+			1: "│%s.",
+			2: "│.┌",
+		},
+	},
+}
+
 type BoardDrawer interface {
+
+	// DrawTo draws the board to the writer.
+	DrawTo(w io.Writer, board *model.Board) (err error)
+}
+
+type boardDrawer struct {
+}
+
+func (d boardDrawer) DrawTo(w io.Writer, board *model.Board) (err error) {
+	for _, line := range board.Tiles {
+		for i := 0; i < 3; i++ {
+			for _, bt := range line {
+				var bytes []byte
+				if i != 1 {
+					bytes = []byte(shapesParts[bt.Tile.Shape][bt.Rotation][i])
+				} else {
+					bytes = []byte(fmt.Sprintf(shapesParts[bt.Tile.Shape][bt.Rotation][i], string(bt.Tile.Treasure)))
+				}
+
+				if _, err := w.Write(bytes); err != nil {
+					return err
+				}
+			}
+
+			if _, err := w.Write([]byte{'\n'}); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func NewBoardDrawer() BoardDrawer {
+	return &boardDrawer{}
 }
--- labyrinth.go
+++ labyrinth.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/marmelab/labyrinth/internal/model"
+	"github.com/marmelab/labyrinth/internal/presentation"
+)
 
 func main() {
-	fmt.Println("Welcome to labyrinth CLI edition.")
+	board, err := model.NewBoard(3)
+	if err != nil {
+		log.Fatalf("Failed to initialize board: %v.", err)
+	}
+
+	drawer := presentation.NewBoardDrawer()
+
+	drawer.DrawTo(os.Stdout, board)
 }
